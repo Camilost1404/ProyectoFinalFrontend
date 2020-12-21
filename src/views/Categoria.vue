5c0c60fa5f59ad25e13475b55ffed2590ef9c1f0
Camilost1404 <camitorres1404@gmail.com>
2020-12-21 01:28:29 -0500

    Rutas categoria

---
+++ src/views/Categoria.vue
@@ -0,0 +1,17 @@
+<template>
+    <component-categoria-table></component-categoria-table>
+</template>
+
+<script>
+import ComponentCategoriaTable from "../components/ComponentCategoriaTable.vue"
+    export default {
+        name: Categoria,
+        components: {
+            ComponentCategoriaTable
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
